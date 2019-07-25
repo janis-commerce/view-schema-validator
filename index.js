dc242f3cf5301df1d989cb5e9f2d049a17edfa1a
alejandro <ale.g.acosta08@gmail.com>
2019-07-25 11:50:32 -0300

    init proyect

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { ViewSchemaValidator } = require('./lib');
+
+module.exports = ViewSchemaValidator;
---
+++ tests/view-schema-validator-test.js
@@ -0,0 +1,13 @@
+/* 'use strict';
+
+const assert = require('assert');
+
+const sandbox = require('sinon').createSandbox();
+
+const { ViewSchemaValidato } = require('./../lib');
+
+describe('ViewSchemaValidator', () => {
+
+	// your tests here...
+
+}); */
