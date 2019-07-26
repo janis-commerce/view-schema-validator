b01a4064107b63d6db71706ad8f74479f5353665
alejandro <ale.g.acosta08@gmail.com>
2019-07-26 19:05:46 -0300

    add initla components for validate schema

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
+#!/usr/bin/env node
+
 'use strict';
+
+const { argv } = require('yargs')
+	.command('build')
+	.command('validate')
+	.demandCommand()
+	.option('input', {
+		alias: 'i',
+		describe: 'write a relative dir for your inputs files folder or file'
+	})
+	.option('output', {
+		alias: 'o',
+		describe: 'write a relative dir for outputs'
+	})
+	.help('help');
+
 
 const { ViewSchemaValidator } = require('./lib');
 
-module.exports = ViewSchemaValidator;
+const availableCommands = ['build', 'validate'];
+
+(() => {
+	const { input, output, _: commands } = argv;
+
+	if(input && output) {
+		const [command] = commands;
+		const currentCommand = availableCommands.find(cmd => cmd === command);
+
+		const schemaValidator = new ViewSchemaValidator(input, output);
+		const execute = schemaValidator.execute.bind(schemaValidator);
+
+		if(currentCommand)
+			execute(currentCommand.toLowerCase());
+	}
+})();
