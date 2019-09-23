25b155d895b7adb121e5f8d58a50e6be3a880491
alejandro <ale.g.acosta08@gmail.com>
2019-09-23 15:41:41 -0300

    fix tests for schema modifier

--- tests/schema-modifier-test.js
+++ tests/schema-modifier-test.js
@@ -53,7 +53,7 @@ describe('Test schema-modifier functions', () => {
 
 		const dataOne = schemaModifier.execute(schemaOne);
 
-		sandbox.assert.match(dataOne, schemaOne);
+		assert.deepEqual(dataOne, schemaOne);
 	});
 
 	it('should pass validation with out changes in edit.json compiled', async () => {
@@ -62,7 +62,7 @@ describe('Test schema-modifier functions', () => {
 		const dataOne = schemaModifier.execute(schemaOne);
 
 		assert(addBrowseActionsSpy.notCalled);
-		sandbox.assert.match(dataOne, schemaOne);
+		assert.deepEqual(dataOne, schemaOne);
 	});
 
 });
