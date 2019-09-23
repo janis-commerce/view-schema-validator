28d057cb157dffd5b7491525462bc4b61c90d9b4
alejandro <ale.g.acosta08@gmail.com>
2019-09-23 14:39:45 -0300

    change defaults actions

--- tests/schema-modifier-test.js
+++ tests/schema-modifier-test.js
@@ -20,7 +20,22 @@ describe('Test schema-modifier functions', () => {
 		const schemaOne = JSON.parse(schemaCompiled.toString());
 		const dataOne = schemaModifier.execute(schemaOne);
 
-		sandbox.assert.match(dataOne, JSON.parse(schemaExpectedOne.toString()));
+		assert.deepEqual(dataOne, JSON.parse(schemaExpectedOne.toString()));
+	});
+
+	it('should pass validation and not add actions default is has empty array', () => {
+		const schemaOne = JSON.parse(schemaCompiled.toString());
+		const schemaTwo = JSON.parse(schemaCompiled.toString());
+		const addBrowseActionsSpy = sandbox.spy(schemaModifier, 'addBrowseActions');
+
+		schemaOne.actions = [];
+
+		const dataOne = schemaModifier.execute(schemaOne);
+
+		schemaTwo.actions = [];
+
+		assert(addBrowseActionsSpy.notCalled);
+		assert.deepEqual(dataOne, schemaTwo);
 	});
 
 	it('should pass validation with out changes in browse.json compiled', async () => {
